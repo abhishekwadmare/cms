7cff69e65d7d93024f18dc568735f4df79b0e2b0
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-19 14:07:03 +0530

    Created repository for cms

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php 
+
+define('DB_HOST',"localhost");
+define('DB_USER',"phpmyadmin");
+define('DB_PASS',"Success");
+define('DB_NAME',"cms");
+
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// if($connection) {
+//     echo "we are connected";
+
+// }
+
+
+
+
+
+
+
+?>
---
+++ includes/sidebar.php
@@ -0,0 +1,55 @@
+<div class="col-md-4">
+
+<!-- Blog Search Well -->
+<div class="well">
+    <h4>Blog Search</h4>
+    <form action = "search.php" method = "post"> 
+        <div class="input-group">
+            <input name = "search" type="text" class="form-control">
+            <span class="input-group-btn">
+                <button name = "submit" class="btn btn-default" type="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
+        </div>
+    </form>
+    <!-- /.input-group -->
+</div>
+
+
+
+
+
+
+
+<!-- Blog Categories Well -->
+<div class="well">
+
+                <?php 
+                    $query = "SELECT * FROM categories";
+                    $select_categories_sidebar = mysqli_query($connection, $query);
+                   
+                ?>
+    <h4>Blog Categories</h4>
+    <div class="row">
+        <div class="col-lg-12">
+            <ul class="list-unstyled">
+                <?php
+                    while($row = mysqli_fetch_assoc($select_categories_sidebar)){
+                            $cat_title = $row['cat_title'];
+                            echo "<li><a href='#'>{$cat_title}</a></li>";
+                        } 
+                ?>
+            </ul>
+        </div>
+
+
+    </div>
+    <!-- /.row -->
+</div>
+
+
+<!-- Side Widget Well -->
+<?php include "widget.php"; ?>
+
+</div>
