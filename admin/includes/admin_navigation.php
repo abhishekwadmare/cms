9c2951f6437d9ada84c0305bd889076c0610df61
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-23 19:39:42 +0530

    Creating a HTML table in admin to display a list of posts

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -38,7 +38,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View all Posts</a>
+                                <a href="posts.php">View all Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
---
+++ admin/posts.php
@@ -0,0 +1,66 @@
+<!-- header -->
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                       <table class="table table-bordered table-hover">
+                           <thead>
+                               <tr>
+                                   <th>Id</th>
+                                   <th>Author</th>
+                                   <th>Title</th>
+                                   <th>Categories</th>
+                                   <th>Status</th>
+                                   <th>Image</th>
+                                   <th>Tags</th>
+                                   <th>Comments</th>
+                                   <th>Date</th>
+                               </tr>
+                           </thead>
+                           <tbody>
+                               <tr>
+                                   <td>10</td>
+                                   <td>Edwin Diaz</td>
+                                   <td>Bootstrap framework</td>
+                                   <td>Bootstrap</td>
+                                   <td>Status</td>
+                                   <td>Image</td>
+                                   <td>Tags</td>
+                                   <td>Comments</td>
+                                   <td>Date</td>
+                               </tr>
+                           </tbody>
+                       </table>
+
+
+
+
+
+                        
+                       
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+        <!-- Footer -->
+        <?php include "includes/admin_footer.php"; ?>
