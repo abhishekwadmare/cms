cb90a8dc5606b720474b369dd4f0213b53fa99b5
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-23 20:19:31 +0530

    Displaying posts list in admin

--- admin/posts.php
+++ admin/posts.php
@@ -32,17 +32,37 @@
                                </tr>
                            </thead>
                            <tbody>
-                               <tr>
-                                   <td>10</td>
-                                   <td>Edwin Diaz</td>
-                                   <td>Bootstrap framework</td>
-                                   <td>Bootstrap</td>
-                                   <td>Status</td>
-                                   <td>Image</td>
-                                   <td>Tags</td>
-                                   <td>Comments</td>
-                                   <td>Date</td>
-                               </tr>
+                            
+                                <?php
+                                    $query = "SELECT * FROM posts";
+                                    $select_posts = mysqli_query($connection, $query);
+                                    while($row = mysqli_fetch_assoc($select_posts)){
+                                        $post_id = $row['post_id'];    
+                                        $post_author = $row['post_author'];
+                                        $post_title = $row['post_title'];
+                                        $post_category_id = $row['post_category_id'];
+                                        $post_status = $row['post_status'];
+                                        $post_image = $row['post_image'];
+                                        $post_tags = $row['post_tags'];
+                                        $post_comment_count = $row['post_comment_count'];
+                                        $post_date = $row['post_date'];
+                                    
+                                        echo "<tr>";
+                                        echo "<td>{$post_id}</td>";
+                                        echo "<td>{$post_author}</td>";
+                                        echo "<td>{$post_title}</td>";
+                                        echo "<td>{$post_category_id}</td>";
+                                        echo "<td>{$post_status}</td>";
+                                        echo "<td><img width = '100' src ='../images/{$post_image}' alt ='image'></td>";
+                                        echo "<td>{$post_tags}</td>";
+                                        echo "<td>{$post_comment_count}</td>";
+                                        echo "<td>{$post_date}</td>";
+                                        echo "</tr>";
+                                    
+                                    }
+                                ?>
+
+
                            </tbody>
                        </table>
 
