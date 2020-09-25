2daeee5a47d9aa3497b48e1b13dfaa0c2ba75377
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-25 19:13:02 +0530

    Finally Updating Posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -18,6 +18,43 @@
         $post_date = $row['post_date'];
     }
 
+    if(isset($_POST['update_post'])){
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_author = $_POST['post_author'];
+        $post_status = $_POST['post_status'];       
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_tags = $_POST['post_tags'];
+        $post_content = $_POST['post_content']; 
+
+        move_uploaded_file($post_image_temp,"../images/{$post_image}");
+
+        if(empty($post_image)){
+
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+            $select_image = mysqli_query($connection,$query);
+
+            while($row = mysqli_fetch_array($select_image)){
+                $post_image = $row['post_image'];
+            }
+        }
+        
+        $query =  "UPDATE posts SET ";
+        $query .= "post_title   = '{$post_title}', ";
+        $query .= "post_category_id = '{$post_category_id}', ";
+        $query .= "post_date    = now(), ";
+        $query .= "post_author  = '{$post_author}', ";
+        $query .= "post_status  = '{$post_status}', ";
+        $query .= "post_tags    = '{$post_tags}', ";
+        $query .= "post_content = '{$post_content}', ";
+        $query .= "post_image = '{$post_image}' ";
+        $query .= "WHERE post_id = '{$the_post_id}' ";
+
+        $update_post = mysqli_query($connection,$query);    
+
+    }
+
 ?>
 
 
@@ -28,26 +65,21 @@
     </div>
     
     <div class="form-group">
-    <select name="post_category" id="">
-        <?php 
+        <select name="post_category" id="">
+            <?php 
+            
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+            confirmQuery($select_categories);
+            while($row = mysqli_fetch_assoc($select_categories)){
+                $cat_id = $row['cat_id'];    
+                $cat_title = $row['cat_title'];
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+            
+            ?>
         
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection, $query);
-           confirmQuery($select_categories);
-        while($row = mysqli_fetch_assoc($select_categories)){
-            $cat_id = $row['cat_id'];    
-            $cat_title = $row['cat_title'];
-            echo "<option value='{$cat_id}'>{$cat_title}</option>";
-        }
-        
-        ?>
-    
-    
-    
-    </select>
-
-
-
+        </select>
 
     </div>
     
@@ -63,6 +95,7 @@
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="image">
+        <input  type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -76,7 +109,7 @@
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" class="form-control" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" class="form-control" name="update_post" value="Update Post">
     </div>
 
 </form>
