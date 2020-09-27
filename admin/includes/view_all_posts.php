86134c31ef7f39c578dabe18b4b77c66a20771f2
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-27 18:02:47 +0530

    Adjustments to visual for comments

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -10,6 +10,8 @@
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
     </thead>
     <tbody>
@@ -68,6 +70,7 @@ if(isset($_GET['delete'])){
    $the_post_id = $_GET['delete'];
    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
    $delete_query = mysqli_query($connection, $query);
+   header("Location: posts.php");
 
 }
 
