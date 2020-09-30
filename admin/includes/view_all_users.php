49c59d62d67c431ed633c8ed5000c79053d59ed6
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-09-30 19:08:29 +0530

    Modifying user table heading

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,15 +2,12 @@
     <thead>
         <tr>
             <th>Id</th>
-            <th>Author</th>
-            <th>Comment</th>
+            <th>Username</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
             <th>Email</th>
-            <th>Status</th>
-            <th>In Response to</th>
+            <th>Role</th>
             <th>Date</th>
-            <th>Approve</th>
-            <th>Unapprove</th>
-            <th>Delete</th>
         </tr>
     </thead>
     <tbody>
