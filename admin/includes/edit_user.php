9c641767c7eb16367c49027762f1df1aa5cb6a8f
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-10-02 19:24:19 +0530

    Updating User

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -39,10 +39,17 @@ if(isset($_POST['edit_user'])){
 
     // move_uploaded_file($post_image_temp,"../images/{$post_image}");
 
-    $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) ";
-    $query .= "VALUES('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}') ";
-    $create_user_query = mysqli_query($connection, $query);
-    confirmQuery($create_user_query);
+    $query =  "UPDATE users SET ";
+    $query .= "user_firstname   = '{$user_firstname}', ";
+    $query .= "user_lastname = '{$user_lastname}', ";
+    $query .= "user_role  = '{$user_role}', ";
+    $query .= "username  = '{$username}', ";
+    $query .= "user_email    = '{$user_email}', ";
+    $query .= "user_password = '{$user_password}' ";
+    $query .= "WHERE user_id = {$the_user_id} ";
+
+    $edit_user_query = mysqli_query($connection,$query);   
+    confirmQuery($edit_user_query);
 }
 
 
