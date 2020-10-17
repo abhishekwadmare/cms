3b89d70642bbc122b655c3905abb870b79835431
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-10-17 10:51:39 +0530

    Making the login page

---
+++ includes/login.php
@@ -0,0 +1,19 @@
+<?php include "db.php"; ?>
+
+<?php
+
+if(isset($_POST['login'])){
+
+    echo $username = $_POST['username'];
+    echo $password = $_POST['password'];
+    
+
+}
+
+
+
+
+
+
+
+?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -24,16 +24,12 @@
     <form action = "includes/login.php" method = "post"> 
         <div class="form-group">
             <input name = "username" type="text" class="form-control" placeholder="Enter Username">
-            
         </div>
 
         <div class="input-group">
             <input name = "password" type="password" class="form-control" placeholder="Enter Password">
             <span class="input-group-btn" >
                 <button class="btn btn-primary" name="login" type="submit"> Submit </button>
-            
-            
-            
             </span>
         </div>
     </form>
