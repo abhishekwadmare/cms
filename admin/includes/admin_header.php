9ca00739ff6f64e57da670ed8398b2e96279777b
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-10-17 12:11:04 +0530

    Validating User Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,28 @@
-<?php ob_start(); ?>
-<?php session_start(); ?>
 <?php include "../includes/db.php";?>
 <?php include "functions.php";?>
+<?php ob_start(); ?>
+<?php session_start(); ?>
+
+<?php
+
+    if(isset($_SESSION['user_role'])){
+        if($_SESSION['user_role'] !== 'admin'){
+            header("Location: ../index.php");
+        }
+    }
+
+
+
+
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
