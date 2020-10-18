6c46f0586a0c2677814dcd79be11a1f65e929fa7
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-10-18 13:16:35 +0530

    Cleaning up the chart a little

--- admin/index.php
+++ admin/index.php
@@ -143,17 +143,18 @@
 
                         function drawChart() {
                             var data = google.visualization.arrayToDataTable([
-                            ['Year', 'Sales', 'Expenses', 'Profit'],
-                            ['2014', 1000, 400, 200],
-                            ['2015', 1170, 460, 250],
-                            ['2016', 660, 1120, 300],
-                            ['2017', 1030, 540, 350]
+                            ['Date', 'Count'],
+
+
+
+
+                            ['Post', 1000],
                             ]);
 
                             var options = {
                             chart: {
-                                title: 'Company Performance',
-                                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                title: '',
+                                subtitle: '',
                             }
                             };
 
@@ -163,7 +164,7 @@
                         }
                     </script>
 
-                    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
 
 
                 </div>
