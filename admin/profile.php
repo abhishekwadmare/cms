3e5ceca8e7446f7387ebd82157e1565cec2a3b81
abhishekwadmare <abhishek.wadmare@gmail.com>
2020-10-18 10:43:28 +0530

    Display User Data

--- admin/profile.php
+++ admin/profile.php
@@ -5,10 +5,23 @@
 
     if(isset($_SESSION['username'])){
         
+        $username = $_SESSION['username'];
+
+        $query = "SELECT * FROM users WHERE username = '{$username}' ";
+        $select_user_profile_query = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_user_profile_query)){
+            $user_id      = $row['user_id'];    
+            $username    = $row['username'];
+            $user_password  = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname   = $row['user_lastname'];
+            $user_email  = $row['user_email'];
+            $user_image    = $row['user_image'];
+            $user_role    = $row['user_role'];
+        }
 
     }
-
-
 
 ?>
 
@@ -81,7 +94,7 @@
                             </div>
 
                             <div class="form-group">
-                                <input class="btn btn-primary" type="submit" class="form-control" name="edit_user" value="Edit User">
+                                <input class="btn btn-primary" type="submit" class="form-control" name="edit_user" value="Update Profile">
                             </div>
 
                         </form>
